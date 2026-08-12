01f544327a7680485eb8051e9e62706c91352213
abnavesayali3 <abnavesayali3@gmail.com>
2026-08-12 11:56:22 +0530

    Time: 0 ms (100%), Space: 58.2 MB (15.49%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+ // Using Recursion
+var mergeTwoLists = function(head1, head2) {
+    if(head1 == null || head2 == null)
+            return head1 == null ? head2 : head1;
+
+    if(head1.val <= head2.val){
+        head1.next = mergeTwoLists(head1.next, head2);
+        return head1;
+    }
+    else {
+         head2.next = mergeTwoLists(head1, head2.next);
+        return head2;
+    }
+};
