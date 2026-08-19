18ede26a735dc42dcf7ff9d49027703353623d31
abnavesayali3 <abnavesayali3@gmail.com>
2026-08-19 12:08:39 +0530

    Time: 1 ms (60.67%), Space: 52.8 MB (97.38%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let x = 0;
+    let len = strs[0].length;
+
+    while (x < len) {
+        let ch = strs[0][x];
+
+        for (let i = 1; i < strs.length; i++) {
+
+            if (ch != strs[i][x]) {
+                return strs[0].substring(0, x);
+            }
+        }
+
+        x++;
+    }
+
+    return strs[0];
+};
